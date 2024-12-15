5b8cf7c38155eb18511474f9514395d75bf85541
zeekomkommer <anouaranthonytel@gmail.com>
2024-12-15 17:34:12 +0100

    Added baseplate

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Code below precisely centers all header items
+
+function resizeHeaderItems() {
+
+    const socials = document.getElementById("socials");
+    const copyright = document.getElementById("copyright");
+
+    const boundCopyright = copyright.getBoundingClientRect();
+    const boundSocials = socials.getBoundingClientRect();
+
+    if (boundSocials.width < boundCopyright.width) {
+        socials.style.margin = "0px " + ((boundCopyright.width - boundSocials.width) / 2) + "px";
+        copyright.style.margin = "0";
+
+    } else {
+        socials.style.margin = "0";
+        copyright.style.margin = "0px " + ((boundSocials.width - boundCopyright.width) / 2) + "px";
+    }
+}
+
+resizeHeaderItems();
+window.addEventListener("resize", resizeHeaderItems);
