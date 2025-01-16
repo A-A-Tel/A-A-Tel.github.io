f55589c0eac18707473aa2ba020d17c3974f36c0
AAT <anouaranthonytel@gmail.com>
2025-01-16 10:20:21 +0100

    Added html and stuff idk

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Preset functions
+
+// function getRandomChar() {
+//     const min = 32;
+//     const max = 255;
+//     return String.fromCharCode(Math.floor(Math.random() * (max - min + 1) + min));
+// }
+
 // Code below precisely centers all header items
 
 function resizeHeaderItems() {
@@ -20,4 +28,25 @@ function resizeHeaderItems() {
 
 resizeHeaderItems();
 window.addEventListener("resize", resizeHeaderItems);
+
+// Random text generator
+
+// const par = document.getElementById("par");
+// const text = par.textContent;
+//
+// function randomText() {
+//     let newText = "";
+//     for (i = 0; i < text.length; i++) {
+//         if (Math.random() > 0.97) {
+//             newText += getRandomChar();
+//         } else {
+//             newText += text.charAt(i);
+//         }
+//     }
+//     par.textContent = newText;
+// }
+//
+// setInterval(() => {
+//     randomText();
+// }, 100)
 
