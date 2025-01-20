d4affec6813bea52efc140503ccdc7b76c03fdbe
AAT <anouaranthonytel@gmail.com>
2025-01-20 10:40:15 +0100

    Made games lol

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// Preset functions
-
-// function getRandomChar() {
-//     const min = 32;
-//     const max = 255;
-//     return String.fromCharCode(Math.floor(Math.random() * (max - min + 1) + min));
-// }
-
-// Code below precisely centers all header items
 
 function resizeHeaderItems() {
 
@@ -17,7 +8,7 @@ function resizeHeaderItems() {
     const boundSocials = socials.getBoundingClientRect();
 
     if (boundSocials.width < boundCopyright.width) {
-        socials.style.margin = "0px " + ((boundCopyright.width - boundSocials.width) / 2) + "px";
+        socials.style.margin = "0 " + ((boundCopyright.width - boundSocials.width) / 2) + "px";
         copyright.style.margin = "0";
 
     } else {
@@ -28,25 +19,3 @@ function resizeHeaderItems() {
 
 resizeHeaderItems();
 window.addEventListener("resize", resizeHeaderItems);
-
-// Random text generator
-
-// const par = document.getElementById("par");
-// const text = par.textContent;
-//
-// function randomText() {
-//     let newText = "";
-//     for (i = 0; i < text.length; i++) {
-//         if (Math.random() > 0.97) {
-//             newText += getRandomChar();
-//         } else {
-//             newText += text.charAt(i);
-//         }
-//     }
-//     par.textContent = newText;
-// }
-//
-// setInterval(() => {
-//     randomText();
-// }, 100)
-
---
+++ js/master-mind.js
@@ -0,0 +1,84 @@
+// Map to convert class suffixes to color tokens
+const colors = new Map();
+colors.set('red', 'R');
+colors.set('green', 'G');
+colors.set('blue', 'B');
+colors.set('yellow', 'Y');
+colors.set('orange', 'O');
+colors.set('cyan', 'C');
+
+const codeItems = ['R', 'G', 'B', 'Y', 'O', 'C'];
+const setPositions = new Array(4);
+setPositions.fill(false);
+
+let secretCode = generateCode();
+let masterMindHasWon = false;
+
+function generateCode() {
+    const code = new Array(4);
+
+    for (let i = 0; i < code.length; i++) {
+        code[i] = codeItems[Math.floor(Math.random() * codeItems.length)];
+    }
+    return code;
+}
+
+function resetGame() {
+    setPositions.fill(false);
+    secretCode = generateCode();
+
+    for (let i = 0; i < setPositions.length; i++) {
+        document.getElementById('eval-' + i).className = 'pin-grey';
+        document.getElementById('player-' + i).className = 'pin-grey';
+        document.getElementById('select-' + i).value = 'none';
+    }
+    masterMindHasWon = false;
+}
+
+function winSequence() {
+    for (let i = 0; i < setPositions.length; i++) {
+        if (!setPositions[i]) {
+            return;
+        }
+    }
+    setPositions.fill(false);
+    let score = 0;
+    for (let i = 0; i < setPositions.length; i++) {
+
+        const selectElement = document.getElementById('select-' + i);
+        const evalElement = document.getElementById('eval-' + i);
+
+        if (colors.get(selectElement.value) === secretCode[i]) {
+
+            evalElement.className = 'pin-black';
+            score++;
+            setPositions[i] = true;
+        } else if (secretCode.includes(colors.get(selectElement.value))) {
+
+            evalElement.className = 'pin-white';
+            selectElement.value = 'none';
+        } else {
+
+            evalElement.className = 'pin-grey';
+            selectElement.value = 'none';
+        }
+    }
+    if (score === 4) {
+        masterMindHasWon = true;
+        setInterval(resetGame, 5000);
+    }
+}
+
+function changeColor(element) {
+
+    if (!masterMindHasWon) {
+
+        const id = element.id.charAt(element.id.length - 1);
+        const color = element.value;
+
+        document.getElementById('player-' + id).className = 'pin-' + color;
+        setPositions[id] = true;
+        winSequence();
+    }
+
+}
